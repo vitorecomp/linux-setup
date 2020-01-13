0e7f9d155da80cb36f8aa513593309138893ee24
vitor <vitor.ecomp@gmail.com>
2020-01-12 23:23:19 -0300

    add the fonts

--- push.py
+++ push.py
@@ -4,6 +4,8 @@ import os
 import shutil 
 from os.path import expanduser
 
+#fazer aoagar a pasta resources aqui
+
 #create the resource folder
 resource_folder = './resource'
 if not os.path.exists(resource_folder):
@@ -23,3 +25,9 @@ os.mkdir(resource_folder+ '/bash')
 destination = resource_folder + '/bash/.zshrc'
 
 shutil.copyfile(source, destination)
+
+#copy fonts folder
+source = home + '/.fonts'
+destination = resource_folder + '/fonts'
+
+shutil.copytree(source, destination)
