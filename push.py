a32215a61e15c10ba29a9626e6007ee3f91487fe
vitor <vitor.ecomp@gmail.com>
2020-01-12 23:19:31 -0300

    Add the zshrc

--- push.py
+++ push.py
@@ -9,10 +9,17 @@ resource_folder = './resource'
 if not os.path.exists(resource_folder):
     os.mkdir(resource_folder)
 	
-#copy the i3 config file
 home = expanduser("~")
+#copy the i3 config file
 source = home + '/.config/i3'
-print(source)
 destination = resource_folder + '/config'
 
 shutil.copytree(source, destination)
+
+
+#copy bash file
+source = home + '/.zshrc'
+os.mkdir(resource_folder+ '/bash')
+destination = resource_folder + '/bash/.zshrc'
+
+shutil.copyfile(source, destination)
